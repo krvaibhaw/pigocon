a261c1a25c4f07879cbb6dee97b99af500a588a0
Vaibhaw <kvaibhaw99@gmail.com>
2021-08-27 11:25:00 -0400

    Update JavaScript

---
+++ script/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Selecting Elements
+const p1 = document.querySelector('.player--0')
+const p2 = document.querySelector('.player--1')
+const score1 = document.querySelector('#score--0')
+const score2 = document.getElementById('score--1')
+const curr1 = document.getElementById('current--0')
+const curr2 = document.getElementById('current--1')
+
