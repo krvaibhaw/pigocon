c0e463a7faefda76c06ba486ddbfb7163bf11552
Vaibhaw <kvaibhaw99@gmail.com>
2021-09-10 11:25:00 -0400

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -12,3 +12,28 @@ const bnew = document.querySelector('.btn--new')
 const broll = document.querySelector('.btn--roll')
 const bhold = document.querySelector('.btn--hold')
 
+
+let currscore, activePlayer, tscore, playing
+
+// Starting conditions
+const init = function () {
+    
+    currscore = 0
+    activePlayer = 0
+    tscore = [0, 0]
+    playing = true
+
+    score1.textContent = 0
+    score2.textContent = 0
+    
+    curr1.textContent = 0
+    curr1.textContent = 0
+    
+    dice.classList.add('hidden')
+
+    p1.classList.add('player--active')
+    p2.classList.remove('player--active')
+
+    p1.classList.remove('player--winner')
+    p2.classList.remove('player--winner')
+}
